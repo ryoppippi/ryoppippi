ae9edde152f1f1a1d9a8c06b937889bd9c911d50
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-02-04 12:40:44 +0000

    chore: add nix flake dev environment
    Add devShell with Node.js 24 and pnpm using mkShellNoCC.

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.nodejs_24
+              pkgs.pnpm
+            ];
+          };
+        }
+      );
+    };
+}
